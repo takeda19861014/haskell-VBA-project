9059bb28d397159abbbd2a6180ed0439f9ba4bc0
takeda tomohiro <takedatomohiro1014@gmail.com>
2025-06-05 18:01:21 +0900

    mapの値をEither型にした”

--- MyLib.hs
+++ MyLib.hs
@@ -4,14 +4,22 @@ module MyDict where
 import Foreign.C.Types
 import qualified Data.Map.Strict as Map
 
-myDict :: Map.Map Int Int
+-- Mapの値をEither String Int型にする
+myDict :: Map.Map Int (Either String Int)
 myDict = Map.fromList [
-    (32, 0),
-    (33, 1),
-    (38, 25),
-    (65293, 1)
+    (32, Right 0),
+    (33, Right 1),
+    (38, Right 25),
+    (65293, Right 1)
   ]
 
+-- C言語から呼び出せるようにエクスポート
 foreign export ccall getDictValue :: CInt -> IO CInt
+
 getDictValue :: CInt -> IO CInt
-getDictValue key = return $ maybe (-1) fromIntegral (Map.lookup (fromIntegral key) myDict)
+getDictValue key =
+  case Map.lookup (fromIntegral key) myDict of
+    Nothing        -> return (-1)  -- キーが存在しない
+    Just (Left _)  -> return (-2)  -- 特別なLeft値（今は未使用）
+    Just (Right v) -> return (fromIntegral v)  -- 通常の値
+
--- wrapper.c
+++ wrapper.c
@@ -8,8 +8,20 @@ extern "C" {
     extern HsInt32 getDictValue(HsInt32 key);
 
     // VBAや他言語から呼び出す窓口
+    // 戻り値の意味:
+    //   >=0  ... Haskell辞書のRight値
+    //   -1   ... Haskell辞書にキーが存在しない
+    //   -2   ... Haskell辞書にLeft値が格納されている
     __declspec(dllexport) int getDictValueWrapper(int key) {
-        return getDictValue(key);
+        int result = getDictValue(key);
+
+        // ここでC側でエラー処理や分岐も可能
+        // 例:
+        // if (result == -1) { /* キーなしの処理 */ }
+        // else if (result == -2) { /* Left値の処理 */ }
+        // else { /* 通常値 */ }
+
+        return result;
     }
 
 #ifdef __cplusplus
